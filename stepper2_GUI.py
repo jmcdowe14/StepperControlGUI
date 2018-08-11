9503bece89a876d0cd999642657539000d04a6e9
jmcdowe14 <mcdowell.jeffrey123@gmail.com>
2018-08-11 20:17:08 -0300

    GUI for stepper controller

---
+++ stepper2_GUI.py
@@ -0,0 +1,50 @@
+import RPi.GPIO as GPIO, time
+
+import sys
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(16, GPIO.OUT)
+
+GPIO.setup(18, GPIO.OUT)
+
+p = GPIO.PWM(16, 100)
+
+def SpinMotor(dire):
+    p.ChangeFrequency(100)
+
+    GPIO.out(18,dire)
+
+    p.start(1)
+
+    time.sleep(0.01)
+
+    return True
+
+while True:
+
+    dir_input = raw_input("Enter your dir: ")
+
+    if dir_input == "f":
+
+        SpinMotor(True)
+
+    elif dir_input == "b":
+
+        SpinMotor(False)
+
+    elif dir_input == "s":
+
+        p.stop()
+
+        dir_input = ""
+
+    elif dir_input == "shutdown":
+
+        p.stop()
+
+        GPIO.cleanup()
+
+        break
+
+
